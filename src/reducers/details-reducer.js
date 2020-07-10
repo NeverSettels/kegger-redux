d84830d4aa1dca4b72cfa527d7e9f1da9b656711
NeverSettels <settels.eric@gmail.com>
2020-07-10 15:11:09 -0700

    add action ttypes to toggle reducers

--- src/reducers/details-reducer.js
+++ src/reducers/details-reducer.js
@@ -1,6 +1,7 @@
+import { TOGGLE_DETAILS } from '../actions/actionTypes'
 export default (state = false, action) => {
   switch (action.type) {
-    case 'TOGGLE_DETAILS':
+    case TOGGLE_DETAILS:
       return !state
     default:
       return state;
--- src/reducers/form-reducer.js
+++ src/reducers/form-reducer.js
@@ -1,6 +1,7 @@
+import { TOGGLE_CREATE } from '../actions/actionTypes'
 export default (state = false, action) => {
   switch (action.type) {
-    case 'TOGGLE_CREATE':
+    case TOGGLE_CREATE:
       return !state
     default:
       return state;
