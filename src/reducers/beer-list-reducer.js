a95e73b856dbe10553d968e1aee0cb2d686e473c
NeverSettels <settels.eric@gmail.com>
2020-07-10 14:53:11 -0700

    add action ttypes to beer list reducer

--- src/reducers/beer-list-reducer.js
+++ src/reducers/beer-list-reducer.js
@@ -1,22 +1,24 @@
+import { ADD_KEG, EDIT_KEG, SERVE, DELETE_KEG } from '../actions/actionTypes'
+
 export default (state = [], action) => {
   const { keg, id } = action;
   switch (action.type) {
-    case 'ADD_KEG':
+    case ADD_KEG:
       const tempState = [...state]
       tempState.push(keg)
       return tempState
-    case "EDIT_KEG":
+    case EDIT_KEG:
       const editState = [...state]
       const editIndex = editState.findIndex(keg => keg.id === id)
       editState[editIndex] = keg;
       return editState
 
-    case "SERVE":
+    case SERVE:
       const serveState = [...state]
       const serveIndex = serveState.findIndex(keg => keg.id === id)
       serveState[serveIndex].pints--;
       return serveState
-    case 'DELETE_KEG':
+    case DELETE_KEG:
       const newState = [...state];
       const deleteIndex = newState.findIndex(keg => keg.id === id)
       newState.splice(deleteIndex, 1)
