0c9b7ef4c9b83ae022caf619e6f32a87b84f169c
NeverSettels <settels.eric@gmail.com>
2020-07-10 13:25:18 -0700

    finished refactor

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,6 @@
+export const ADD_KEG = 'ADD_KEG';
+export const EDIT_KEG = 'EDIT_KEG';
+export const SERVE = 'SERVE';
+export const DELETE_KEG = 'DELETE_KEG';
+export const TOGGLE_DETAILS = 'TOGGLE_DETAILS';
+export const TOGGLE_CREATE = 'TOGGLE_CREATE';
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
-export const toggleDetails = () => ({ type: 'TOGGLE_DETAILS' })
-export const toggleCreate = () => ({ type: 'TOGGLE_CREATE' })
-export const addKeg = keg => ({ type: 'ADD_KEG', keg })
-export const serve = id => ({ type: 'SERVE', id })
-export const editKeg = (keg, id) => ({ type: 'EDIT_KEG', keg, id })
-export const deleteKeg = id => ({ type: 'DELETE_KEG', id })
+import * as types from './actionTypes'
+
+export const toggleDetails = () => ({ type: types.TOGGLE_DETAILS })
+export const toggleCreate = () => ({ type: types.TOGGLE_CREATE })
+export const addKeg = keg => ({ type: types.ADD_KEG, keg })
+export const serve = id => ({ type: types.SERVE, id })
+export const editKeg = (keg, id) => ({ type: types.EDIT_KEG, keg, id })
+export const deleteKeg = id => ({ type: types.DELETE_KEG, id })
