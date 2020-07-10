9e58097f60f941dacc3599e776062e7078482b91
NeverSettels <settels.eric@gmail.com>
2020-07-10 13:42:26 -0700

    rolled back failed refactor

--- src/components/BreweryControl.js
+++ src/components/BreweryControl.js
@@ -4,35 +4,36 @@ import BeerList from './BeerList'
 import { connect } from "react-redux";
 import * as a from '../actions'
 
-function BreweryControl(props) {
-  const showModal = () => this.props.dispatch(a.toggleCreate())
+class BreweryControl extends Component {
+  showModal = () => this.props.dispatch(a.toggleCreate())
 
-  const showDetailModal = () => this.props.dispatch(a.toggleDetails())
+  showDetailModal = () => this.props.dispatch(a.toggleDetails())
 
-  const handleSubmit = (newKeg) => {
+  handleSubmit = (newKeg) => {
     this.props.dispatch(a.addKeg(newKeg))
     this.props.dispatch(a.toggleCreate())
   };
 
-  const handleEdit = (keg, id) => this.props.dispatch(a.editKeg(keg, id))
+  handleEdit = (keg, id) => this.props.dispatch(a.editKeg(keg, id))
 
-  const handleCancel = () => this.props.dispatch(a.toggleCreate())
+  handleCancel = () => this.props.dispatch(a.toggleCreate())
 
-  const handleDetailClose = () => this.props.dispatch(a.toggleDetails())
+  handleDetailClose = () => this.props.dispatch(a.toggleDetails())
 
-  const servePint = id => this.props.dispatch(a.serve(id))
+  servePint = id => this.props.dispatch(a.serve(id))
 
-  const deleteKeg = id => this.props.dispatch(a.deleteKeg(id))
+  delete = id => this.props.dispatch(a.deleteKeg(id))
 
-  return (
-    <>
-      <Header handleCancel={handleCancel} handleOk={handleSubmit} showModal={showModal} visible={this.props.formCreate} />
-      <div className="main-body">
-        <BeerList handleDelete={deleteKeg} servePint={servePint} beerList={this.props.beerList} visible={this.props.details} handleEdit={handleEdit} handleCancel={handleDetailClose} showModal={showDetailModal} />
-      </div>
-    </>
-  )
-
+  render() {
+    return (
+      <>
+        <Header handleCancel={this.handleCancel} handleOk={this.handleSubmit} showModal={this.showModal} visible={this.props.formCreate} />
+        <div className="main-body">
+          <BeerList handleDelete={this.delete} servePint={this.servePint} beerList={this.props.beerList} visible={this.props.details} handleEdit={this.handleEdit} handleCancel={this.handleDetailClose} showModal={this.showDetailModal} />
+        </div>
+      </>
+    )
+  }
 }
 const mapStateToProps = state => {
   return {
